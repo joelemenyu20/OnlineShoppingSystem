1f44d9acd18d24212f552088fac2f6685d8fc2d7
joelemenyu20 <emenyujoel2000@gmail.com>
2022-08-10 12:05:40 -0700

    my commit

---
+++ shoppingsystem.sql
@@ -0,0 +1,211 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 10, 2022 at 04:51 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shoppingsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `id` bigint(20) NOT NULL,
+  `added_date` datetime NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`id`, `added_date`, `email`, `password`, `name`) VALUES
+(1, '0000-00-00 00:00:00', 'admin@gmail.com', 'admin', 'DataFlair');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcart`
+--
+
+CREATE TABLE `tblcart` (
+  `id` bigint(20) NOT NULL,
+  `discount_price` varchar(200) DEFAULT NULL,
+  `quantity` int(11) NOT NULL,
+  `total_price` varchar(200) DEFAULT NULL,
+  `customer_id` bigint(20) NOT NULL,
+  `product_id` bigint(20) NOT NULL,
+  `mrp_price` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblcart`
+--
+
+INSERT INTO `tblcart` (`id`, `discount_price`, `quantity`, `total_price`, `customer_id`, `product_id`, `mrp_price`) VALUES
+(1, '10000', 2, '20000', 1, 1, '30000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcustomer`
+--
+
+CREATE TABLE `tblcustomer` (
+  `id` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `added_date` timestamp NULL DEFAULT current_timestamp(),
+  `email` varchar(100) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `phone` varchar(200) NOT NULL,
+  `pin_code` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblcustomer`
+--
+
+INSERT INTO `tblcustomer` (`id`, `address`, `added_date`, `email`, `gender`, `name`, `password`, `phone`, `pin_code`) VALUES
+(1, 'kampala', '2022-08-10 12:18:59', 'emenyujoel2000@gmail.com', 'M', 'joel', '1234', '0700511907', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblorders`
+--
+
+CREATE TABLE `tblorders` (
+  `id` int(11) NOT NULL,
+  `order_no` int(11) DEFAULT NULL,
+  `customer_name` varchar(200) DEFAULT NULL,
+  `mobile_number` varchar(100) DEFAULT NULL,
+  `email_id` varchar(100) DEFAULT NULL,
+  `address` varchar(400) DEFAULT NULL,
+  `address_type` varchar(100) DEFAULT NULL,
+  `pincode` varchar(100) DEFAULT NULL,
+  `image` varchar(200) DEFAULT NULL,
+  `product_name` varchar(400) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `product_price` varchar(100) DEFAULT NULL,
+  `product_selling_price` varchar(100) DEFAULT NULL,
+  `product_total_price` varchar(100) DEFAULT NULL,
+  `order_status` varchar(100) DEFAULT NULL,
+  `order_date` timestamp NULL DEFAULT current_timestamp(),
+  `payment_mode` varchar(100) DEFAULT NULL,
+  `payment_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblproduct`
+--
+
+CREATE TABLE `tblproduct` (
+  `id` bigint(20) NOT NULL,
+  `active` varchar(100) DEFAULT NULL,
+  `code` varchar(5) DEFAULT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `description` varchar(255) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `image_name` varchar(400) DEFAULT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `price` varchar(200) DEFAULT NULL,
+  `mrp_price` varchar(200) DEFAULT NULL,
+  `product_category` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblcart`
+--
+ALTER TABLE `tblcart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblcustomer`
+--
+ALTER TABLE `tblcustomer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblorders`
+--
+ALTER TABLE `tblorders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblproduct`
+--
+ALTER TABLE `tblproduct`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblcart`
+--
+ALTER TABLE `tblcart`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblcustomer`
+--
+ALTER TABLE `tblcustomer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblorders`
+--
+ALTER TABLE `tblorders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tblproduct`
+--
+ALTER TABLE `tblproduct`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
